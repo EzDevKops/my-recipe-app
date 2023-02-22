7dc1e1c0336edc3fe9c39d5366a884dac516212f
EzDevKops <nicolinekops@hotmail.com>
2023-02-22 12:56:55 +0100

    created the recipe-list. Right before I try myself to auto generate data

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {Recipe} from "../recipe.model";
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,5 +7,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent {
+  recipes: Recipe[] =[
+    new Recipe('A test Recipe', 'This is simply a test', 'https://data.thefeedfeed.com/static/2019/06/14/15605265545d03beda5eb6f.webp')
+  ];
+
+  constructor () {}
+
+ngOnInIt () {}
 
 }
